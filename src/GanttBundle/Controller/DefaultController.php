735cb851ae9f705b9b4349ded0e5b0d620aadbc1
jgondellier <jgondellier@yahoo.fr>
2016-09-19 22:14:25 +0200

    First commit

---
+++ src/GanttBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GanttBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('GanttBundle:Default:index.html.twig');
+    }
+}
---
+++ src/GanttBundle/GanttBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GanttBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GanttBundle extends Bundle
+{
+}
