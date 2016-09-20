10af71664b3518247cea4fe9713ff8504f4f45c6
jgondellier <jgondellier@yahoo.fr>
2016-09-20 22:43:53 +0200

    init

---
+++ src/GanttBundle/Controller/DataController.php
@@ -0,0 +1,29 @@
+<?php
+namespace GanttBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use GanttBundle\Models\JSONGanttConnector;
+
+class DataController extends Controller
+{
+    /**
+     * @Route("/gantt/getdata")
+     */
+    public function indexAction()
+    {
+
+        $res=mysql_connect("localhost","root","");
+        mysql_select_db("gantt");
+
+        $gantt = new JSONGanttConnector($res);
+        $gantt->render_links("gantt_links","id","source,target,type");
+
+        return $gantt->render_table(
+            "gantt_tasks",
+            "id",
+            "start_date,duration,text,progress,sortorder,parent"
+        );
+    }
+}
