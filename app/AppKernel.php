e5ad7a6285fb81c85edd48333d9573d9454f5e64
jgondellier <jgondellier@yahoo.fr>
2016-10-09 21:54:45 +0200

    init

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new GanttBundle\GanttBundle(),
             new IndicateursBundle\IndicateursBundle(),
+            new Ob\HighchartsBundle\ObHighchartsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
