bb145067c5b45f482fdc90103f965d81a6733d31
jgondellier <jgondellier@yahoo.fr>
2017-09-04 17:50:27 +0200

    init

---
+++ src/ProjectBundle/Form/ActivityCommentType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ProjectBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActivityCommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('dateCreation')->add('libelle')->add('auteur')->add('Activity');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProjectBundle\Entity\ActivityComment'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'projectbundle_activitycomment';
+    }
+
+
+}
---
+++ src/ProjectBundle/Form/ProjectType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ProjectBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('projectId')->add('name')->add('codeUrl');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProjectBundle\Entity\Project'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'projectbundle_project';
+    }
+
+
+}
